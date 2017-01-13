1558b2de36ae8d1700d42409689b5fa24601e282
abdullah samman <abdullah.samman@mokus.io>
2017-01-13 12:05:40 +0200

    the right undo

--- copyblokky.chrome.tampermonkey.js
+++ copyblokky.chrome.tampermonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Sublime Text Column Selection
+// @name         Sublime Column Selection
 // @version      0.1
 // @description  try to take over the world!
-// @author       Abdullah Samman
+// @author       Evex
 // @match        *://*/*
 // ==/UserScript==
 
@@ -15,48 +15,33 @@
     var text = "";
     var sel = window.getSelection();
 
-    function make_element(tagname="div", style=""){
-      elem = document.createElement(tagname);
-      elem.setAttribute(`style: ${style}`)
-      return elem;
-    }
+    var sel_underlay = document.createElement('div');
+    sel_underlay.style.backgroundColor = "#b1d7fe";
+    sel_underlay.style.position = "fixed";
+    sel_underlay.id = "sel";
 
-    function init_selection_overlay(){
-      var selection_overlay = document.createElement('div');
-      selection_overlay.id = "sel";
-    }
-    
-  	function init_copy_div(){
-      var copy_div = document.createElement('div');
-      copy_div.style.display = "hidden";
-      copy_div.id = "copy";
-    }
+  var copy_div = document.createElement('div');
+    copy_div.style.display = "hidden";
+    copy_div.id = "copy";
 
-    function init(){
-      var body = document.getElementsByTagName('body')[0];
-      body.appendChild(copy_div);
-      body.appendChild(selection_overlay);
-    }
-
-    function set_and_make_selection_overlay_style(sel_pos){
-      selection_overlay.style.display = "initial";
-      selection_overlay.style.height = sel_pos.height;
-      selection_overlay.style.width = sel_pos.width;
-      selection_overlay.style.top = sel_pos.y;
-      selection_overlay.style.left = sel_pos.x;
-      return `background-color:#b1d7fe;position:fixed;z-index:999999;opacity:0.7;display:block;width:${sel_pos.width}px;height:${sel_pos.height}px;top:${sel_pos.y}px;left:${sel_pos.x}px;`;
-    }
-
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copy_div);
+    body.appendChild(sel_underlay);
 
     var onMouseMove = function(e) {
       if (startPos.x && startPos.y) {
-      	sel_pos = {
-      		x: Math.min(e.x, startPos.x),
-      		y: Math.min(e.y, startPos.y),
-      		height: Math.abs(e.y - startPos.y),
-      		width: Math.abs(e.x - startPos.x)
-      	};
-        selection_overlay.setAttribute("style", set_and_make_selection_overlay_style(sel_pos));
+        sel_pos = {
+          x: Math.min(e.x, startPos.x),
+          y: Math.min(e.y, startPos.y),
+          height: Math.abs(e.y - startPos.y),
+          width: Math.abs(e.x - startPos.x)
+        };
+        sel_underlay.style.display = "initial";
+        sel_underlay.style.height = sel_pos.height;
+        sel_underlay.style.width = sel_pos.width;
+        sel_underlay.style.top = sel_pos.y;
+        sel_underlay.style.left = sel_pos.x;
+        sel_underlay.setAttribute("style", `background-color:#b1d7fe;position:fixed;z-index:999999;opacity:0.7;display:block;width:${sel_pos.width}px;height:${sel_pos.height}px;top:${sel_pos.y}px;left:${sel_pos.x}px;`);
       }
     };
 
@@ -67,21 +52,21 @@
       };
 
       if (startPos && stopPos && startPos.x != stopPos.x && startPos.y != stopPos.y) {
-  		startPosFinal = {
-  			x: Math.min(startPos.x, stopPos.x),
-  			y: Math.min(startPos.y, stopPos.y)
-  		};
-  		stopPosFinal = {
-  			x: Math.max(startPos.x, stopPos.x),
-  			y: Math.max(startPos.y, stopPos.y)
-  		};
+      startPosFinal = {
+        x: Math.min(startPos.x, stopPos.x),
+        y: Math.min(startPos.y, stopPos.y)
+      };
+      stopPosFinal = {
+        x: Math.max(startPos.x, stopPos.x),
+        y: Math.max(startPos.y, stopPos.y)
+      };
         text = "";
-        selection_overlay.style.display = "none";
+        sel_underlay.style.display = "none";
         target_elem = document.elementFromPoint(startPosFinal.x, startPosFinal.y);
         fontSize = parseFloat(
-        	window.getComputedStyle(target_elem).getPropertyValue('font-size'));
+          window.getComputedStyle(target_elem).getPropertyValue('font-size'));
         for (var i = startPosFinal.y + (fontSize/2); i <= stopPosFinal.y; i = i + fontSize) {
-		  target_elem = document.elementFromPoint(startPosFinal.x, i);
+      target_elem = document.elementFromPoint(startPosFinal.x, i);
           start = document.caretRangeFromPoint(startPosFinal.x, i);
           end = document.caretRangeFromPoint(stopPosFinal.x, i);
           range = document.createRange();
@@ -91,11 +76,11 @@
           sel.addRange(range);
           selected_text = sel.toString();
           if (selected_text){
-	          text += selected_text + "<br>";
+            text += selected_text + "<br>";
           }
           fontSize = parseFloat(
-        	window.getComputedStyle(target_elem)
-	        	.getPropertyValue('font-size'));
+          window.getComputedStyle(target_elem)
+            .getPropertyValue('font-size'));
         }
         sel.removeAllRanges();
         copy_div.innerHTML = text;
